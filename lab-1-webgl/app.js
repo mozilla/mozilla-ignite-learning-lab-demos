84550ab03e35b532664c7bc2d311fd2d1d873088
Kenny Katzgrau <katzgrau@gmail.com>
2012-09-04 21:39:03 -0400

    Single file strategy, much faster

--- lab-1-webgl/app.js
+++ lab-1-webgl/app.js
@@ -30,12 +30,7 @@ io.sockets.on('connection', function (socket) {
   	if(started) return; else started = true;
 
   	// TODO: Base this off the level-of-detail param
-  	var path = __dirname + '/datasets/1-percent/';
-
-  	// Get all of the relevant dataset files
-  	var files = fs.readdirSync(path).sort(function(a,b) {
-  		return parseInt(a)-parseInt(b)
-  	});
+  	var path = __dirname + '/datasets/1-percent/frames.csv';
 
   	// The current frame count
   	var count = 0;
@@ -45,28 +40,27 @@ io.sockets.on('connection', function (socket) {
 
   	// How often we send a frame to the client
   	// Ideally, we'd like to stay at 30 fps
-  	var throttle = 30; 
+  	var throttle = 10; 
 
-  	var sendFrame = function() {
-  		 var file = path + files[count];
-  		 rows = [];
+    console.log('Beginning frame sending (unthrottled)');
 
-  		 // Open the CSV, parse it, send it
-		 csv().fromPath(file)
-		 .on('data', function(data,index){
-				rows.push(data);
-		 })
-		 .on('end', function() {
-		 	console.log('Sending frame from ' + file);
-		 	//console.log(rows);
-			socket.emit('frame', {index: count, frame: rows});
-			setTimeout(sendFrame, throttle);
-		 });
+    // Open the CSV, parse it
+     csv().fromPath(path)
+     .on('data', function(data,index){
+        if(data[0] === 'eof') {
+          //console.log('Sending frame from');
+          //console.log(rows);
+          socket.emit('frame', {index: count, frame: rows});
 
-		 count++;
-  	}
+          count++;
+          rows = [];
+        }
 
-  	sendFrame();
+        rows.push(data);
+     })
+     .on('end', function() {
+        console.log('Done sending rows')
+     });
   });
  
 });
