24580c6a47d37665182385a2457f47eae29b5cd7
Joël Cox <joel@joelcox.nl>
2013-01-21 10:59:04 +0100

    Set up basic structure for frontend JS.

--- lab-5-spdy/assets/js/general.js
+++ lab-5-spdy/assets/js/general.js
@@ -3,6 +3,64 @@
  * General frontend JS
  */
 
-(function() {
+(function($) {
+
+	var Loader = {
+
+		settings: {
+			spdyEnabled: true,
+			imagesToLoad: 20,
+		},
+
+		// Are we already busy fetching new images?
+		loading: false,
+
+		// Timestamp when this loading cycle started
+		loadingCycleStart: 0,
+
+		// Bootstrap the module
+		init: function() {
+			this.bindUIActions();
+		},
+
+		// Set up events for both submitting the form, as well as scrolling
+		// down the viewport
+		bindUIActions: function() {
+
+			$('form').on('submit', function(e) {
+				e.preventDefault();
+				Loader.processForm();
+			});
+
+			$(window).on('scroll', function() {
+				var windowPosition = $(this).scrollTop() + $(this).height();
+				var loadAfter = $(document).height() - 100;
+
+				// Are we approaching the bottom of the window yet?
+				if (windowPosition > loadAfter) Loader.processForm();
+	
+			});
+
+		},
+
+		// Retrieve our loading settings from the form and put the
+		// module in loading state
+		processForm: function() {
+			if (this.loading === true) return false;
+			this.loading = true;
+
+			this.settings.spdyEnabled = $('#spdy-enabled');
+			this.settings.imagesToLoad = $('#images-to-load').val();
+
+			console.log('Processed form');
+		},
+
+	}
+
+	$(document).ready(function() {
+		Loader.init();
+	});
+
 	console.log('Welcome to the Mozilla Ignite Learning Labs SPDY Demo')
-})();
+
+})(jQuery);
