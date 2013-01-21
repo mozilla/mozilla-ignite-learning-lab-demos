a93fcf244b4fbebae784f6c3dd9f06a319cacb83
Joël Cox <joel@joelcox.nl>
2013-01-21 12:14:55 +0100

    Added basic loading mechanism for loading the images and timing.

--- lab-5-spdy/assets/js/general.js
+++ lab-5-spdy/assets/js/general.js
@@ -12,8 +12,8 @@
 			imagesToLoad: 20,
 		},
 
-		// Are we already busy fetching new images?
-		loading: false,
+		// Images still left to load
+		remainingImages: 0,
 
 		// Timestamp when this loading cycle started
 		loadingCycleStart: 0,
@@ -47,16 +47,53 @@
 		processForm: function() {
 
 			// Make sure we're not already loading images
-			if (this.loading === true) return false;
-			
-			this.loading = true;
+			if (this.remainingImages !== 0) return false;
 			$checkbox = $('#spdy-enabled');
 
 			this.settings.spdyEnabled = $checkbox.is(':checked') ? true : false;
-			this.settings.imagesToLoad = $('#images-to-load').val();
+			this.settings.imagesToLoad = parseInt($('#images-to-load').val(), 10);
+			this.remainingImages = this.settings.imagesToLoad;
+
+			// And off we go!
+			this.loadImages();
+		},
+
+		// Load the desired amount of images
+		loadImages: function() {
+			this.loadingCycleStart = Date.now();
+
+			// Decrease our counter for every image we load, so we can keep
+			// track of our progress
+			for (var i = this.settings.imagesToLoad; i > 0; i--) {
+
+				var rand = getRandomInt();
+
+				// Construct a new image and bind the event. We can safely
+				// use the load method, because each image has a random int
+				// appended to it and therefore won't be cached.
+				var $el = $('<img />').attr('id', rand);
+				$('#column-2').append($el);
+
+				$('#' + rand).load(this.checkRemainingImages());
+				$('#' + rand).attr('src', 'http://localhost/large.jpg?rand=' + rand);
+
+			}
+
+		},
+
+		checkRemainingImages: function(el) {
+			this.remainingImages--;
+			if (this.remainingImages === 0) console.log('Finished loading');
 		},
 
 	}
+
+	function getRandomInt() {
+		var min = 1000000;
+		var max = 9999999;
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
 
 	$(document).ready(function() {
 		Loader.init();
